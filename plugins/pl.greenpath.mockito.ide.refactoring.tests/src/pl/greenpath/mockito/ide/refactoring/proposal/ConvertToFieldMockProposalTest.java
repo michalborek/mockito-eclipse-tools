fc2bc98210995107044d390b8d60e3ebc677c950
Michal Borek <michal@greenpath.pl>
2014-04-04 18:38:29 +0200

    Reused checkers methods.

--- plugins/pl.greenpath.mockito.ide.refactoring.tests/src/pl/greenpath/mockito/ide/refactoring/proposal/ConvertToFieldMockProposalTest.java
+++ plugins/pl.greenpath.mockito.ide.refactoring.tests/src/pl/greenpath/mockito/ide/refactoring/proposal/ConvertToFieldMockProposalTest.java
@@ -6,22 +6,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
 
-import java.util.List;
-
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.dom.CompilationUnit;
-import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.IAnnotationBinding;
 import org.eclipse.jdt.core.dom.ITypeBinding;
-import org.eclipse.jdt.core.dom.MarkerAnnotation;
-import org.eclipse.jdt.core.dom.Modifier;
-import org.eclipse.jdt.core.dom.Modifier.ModifierKeyword;
-import org.eclipse.jdt.core.dom.SimpleType;
-import org.eclipse.jdt.core.dom.SingleMemberAnnotation;
 import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
-import org.eclipse.jdt.core.dom.TypeLiteral;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
 import org.eclipse.jdt.core.dom.VariableDeclarationStatement;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
@@ -31,6 +22,7 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import pl.greenpath.mockito.ide.refactoring.ConversionsChecker;
 import pl.greenpath.mockito.ide.refactoring.TestUtils;
 import pl.greenpath.mockito.ide.refactoring.ast.AstResolver;
 import pl.greenpath.mockito.ide.refactoring.ast.BindingFinder;
@@ -92,36 +84,7 @@ public class ConvertToFieldMockProposalTest {
 
         final ASTRewrite rewrite = testedClass.getRewrite();
         final TypeDeclaration typeDeclaration = new AstResolver().findParentOfType(statement, TypeDeclaration.class);
-        checkIfFieldWithMockHasBeenAdded(rewrite, typeDeclaration);
-        checkIfRunWithAnnotationIsProperlyAdded(rewrite, typeDeclaration);
-
-    }
-
-    @SuppressWarnings("rawtypes")
-    private void checkIfFieldWithMockHasBeenAdded(final ASTRewrite rewrite, final TypeDeclaration typeDeclaration) {
-        final List bodyDeclarations = rewrite.getListRewrite(typeDeclaration,
-                TypeDeclaration.BODY_DECLARATIONS_PROPERTY).getRewrittenList();
-        assertThat(bodyDeclarations.get(0)).isInstanceOf(FieldDeclaration.class);
-        final FieldDeclaration field = (FieldDeclaration) bodyDeclarations.get(0);
-        assertThat(((SimpleType) field.getType()).getName().getFullyQualifiedName()).isEqualTo("FooBar");
-        assertThat(((Modifier) field.modifiers().get(0)).getKeyword()).isEqualTo(ModifierKeyword.PRIVATE_KEYWORD);
-        assertThat(((VariableDeclarationFragment) field.fragments().get(0)).getName().getFullyQualifiedName())
-                .isEqualTo("fooMock");
-        final List fieldModifiers = rewrite.getListRewrite(field, FieldDeclaration.MODIFIERS2_PROPERTY)
-                .getRewrittenList();
-        assertThat(((MarkerAnnotation) fieldModifiers.get(0)).getTypeName().getFullyQualifiedName()).isEqualTo("Mock");
-    }
-
-    @SuppressWarnings("rawtypes")
-    private void checkIfRunWithAnnotationIsProperlyAdded(final ASTRewrite rewrite, final TypeDeclaration typeDeclaration) {
-        final List rewrittenModifiers = rewrite.getListRewrite(typeDeclaration, TypeDeclaration.MODIFIERS2_PROPERTY)
-                .getRewrittenList();
-        assertThat(rewrittenModifiers.get(0)).isInstanceOf(SingleMemberAnnotation.class);
-        final SingleMemberAnnotation addedAnnotation = (SingleMemberAnnotation) rewrittenModifiers.get(0);
-        assertThat(addedAnnotation.getTypeName().getFullyQualifiedName()).isEqualTo("RunWith");
-        assertThat(addedAnnotation.getValue()).isInstanceOf(TypeLiteral.class);
-        final TypeLiteral parameter = (TypeLiteral) addedAnnotation.getValue();
-        assertThat(((SimpleType) parameter.getType()).getName().getFullyQualifiedName())
-                .isEqualTo("MockitoJUnitRunner");
+        ConversionsChecker.checkIfFieldWithMockHasBeenAdded(rewrite, typeDeclaration, "fooMock", "FooBar");
+        ConversionsChecker.checkIfRunWithAnnotationIsProperlyAdded(rewrite, typeDeclaration);
     }
 }
